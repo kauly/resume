ffe11b18cd65f8248e3cf4a50f0bc8eb7dc153df
kauly <kauly@hotmail.com>
2023-02-08 18:33:31 -0300

    feat: vercel-og

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,10 @@ export default function Head() {
       <title>Kauly's Resume</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="My online resume" />
+      <meta
+        property="og:image"
+        content="https://resume-kappa-indol.vercel.app/api/og"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
