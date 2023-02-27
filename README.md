9f62133c181c7f1d1495e65e0ddadc349c880bd0
kauly <kauly@hotmail.com>
2023-02-27 09:06:29 -0300

    fix: mobile download

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ This is my online resume. It's simple but has some cool features:
 - Server Components
 - Storybook
 - Hand made components
+- Vercel OG
 
-It's a working in progress though.
+# Demo
+
+https://resume-kappa-indol.vercel.app/
--- app/components/PrintButton/PrintButton.tsx
+++ app/components/PrintButton/PrintButton.tsx
@@ -2,12 +2,37 @@
 
 import Icon from "@/app/components/Icon/Icon";
 import Button from "@/app/components/Button/Button";
-
 import printIcon from "@/public/icons/print.svg";
+
+const isMobile = () => window.matchMedia && window.matchMedia("(max-width: 480px)").matches
+
+const downloadFile = () => {
+  const link = document.createElement("a");
+  link.style.display = "none";
+  link.href = 'Kauly_Resume.pdf'
+  link.download = 'Kauly_Resume.pdf';
+
+  document.body.appendChild(link);
+  link.click();
+
+  setTimeout(() => {
+    if(link.parentNode) {
+      link.parentNode.removeChild(link);
+    }
+  }, 0);
+}
+
+const handlePrint = () => {
+  if (isMobile()) {
+    downloadFile()
+  } else {
+    window.print()
+  }
+}
 
 export default function PrintButton() {
   return (
-    <Button size="small" variant="ghost" onClick={() => window.print()}>
+    <Button size="small" variant="ghost" onClick={handlePrint}>
       <Icon src={printIcon} alt="Print Icon" />
     </Button>
   );
