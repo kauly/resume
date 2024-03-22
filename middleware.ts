6e554af6d991c76fc492db3c7b12f4c00015f2fc
kauly <kauly@hotmail.com>
2024-03-22 09:09:09 -0300

    feat: rewrite locale strategy

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import { NextRequest } from "next/server";
 const I18nMiddleware = createI18nMiddleware({
   locales: ["en", "pt"],
   defaultLocale: "en",
+  urlMappingStrategy: "rewrite",
 });
 
 export function middleware(request: NextRequest) {
